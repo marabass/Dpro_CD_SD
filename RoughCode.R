779cb921d22d6e54a85408f78270f0af2cba1334
Brett-VB <brettvanbakel@gmail.com>
2024-03-23 16:44:22 -0400

    Very very early code

--- RoughCode.R
+++ RoughCode.R
@@ -1,3 +1,15 @@
+##Setup
 library(readr)
+library(glmmTMB)
+library(lme4)
 
 MainData <- read.csv("MP_SpeciesStarvation_Clean.csv")
+saveRDS(MainData, file = "MainData.rds")
+MainData <- read_rds("MainData.rds")
+
+##Model Creation, starting with very simple linear model just as a start, will update it to a more accurate model later
+lm1 <- lm(leg_tibL~species_strain*block, data = MainData)
+plot(lm1)
+
+
+
