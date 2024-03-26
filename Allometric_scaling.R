a70f380be58cd00f23ba722c28d267bb814151ce
Brett-VB <brettvanbakel@gmail.com>
2024-03-26 09:52:30 -0400

    no change

--- Allometric_scaling.R
+++ Allometric_scaling.R
@@ -11,7 +11,6 @@ DmelFull_size <- read.csv("MP_SpeciesStarvation_Clean.csv")
 #extracting D prol data 
 Dprol_size <- DmelFull_size[DmelFull_size$species_full == "D_prolongata",]
 
-
 #will need to convert character vectors to factors - Should probably have another script that does this (i.e., data clean up)
 Dprol_size$sex <- as.factor(Dprol_size$sex)
 Dprol_size$condition <- as.factor(Dprol_size$condition)
