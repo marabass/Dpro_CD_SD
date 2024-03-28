8d6b2c2485c0f942baedfb20400390fa7d80a787
Mariam Abass <abassm2@mcmaster.ca>
2024-03-28 11:16:46 -0400

    script for models

---
+++ models.R
@@ -0,0 +1,4 @@
+# response: total size (PC1) - lm(PC1 ~ sex + condition + (1|block))
+# response: tibia length -  lm(leg_log_tibL ~ sex + condtion) 
+# response: tibia width - lm(leg_log_tibW ~ sex + condition)
+#response: leg_log_tar1L - lm(leg_log_tar1L ~ sex + condition)
