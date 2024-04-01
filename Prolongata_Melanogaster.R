7af8db3adca0f383054cbce20a0b284b8f17e352
Brett-VB <brettvanbakel@gmail.com>
2024-04-01 11:54:46 -0400

    Prolongata shape/size analysis repeated with melanogaster

---
+++ Prolongata_Melanogaster.R
@@ -0,0 +1,37 @@
+library(readr)
+library(lme4)
+library(Matrix)
+library(ggplot2)
+library(tidyverse)
+library(dotwhisker)
+
+Dmel_size <- readRDS("Dmel_size.rds")
+Dprol_size <- readRDS("Dprol_size.rds")
+melprol_size <- readRDS("melprol_size.rds")
+
+##Doing data analysis with melanogaster, might be unnecessary depending on how we end up doing second step of analysis
+Dmel_trait_size <- select(Dmel_size, leg_tibL, leg_tibW, leg_tar1L, thorax_length_mm) 
+Dmel_PC <- prcomp(Dmel_trait_size)
+
+ggplot(Dmel_trait_size, aes(y = Dmel_PC$x[,1], x = Dmel_PC$x[,2])) +
+  geom_point() +
+  xlim(-2, 2) +
+  ylim(-2, 2) 
+
+ggplot(Dmel_trait_size, aes(y = Dmel_PC$x[,2], x = Dmel_PC$x[,3])) +
+  geom_point() +
+  xlim(-2, 2) +
+  ylim(-2, 2) 
+
+Dmel_shapVar <- Dmel_PC$x[,1]
+
+lm_total_size_Dmel <- lm(Dmel_shapVar ~ sex * condition, data = Dmel_size)
+plot(lm_total_size)
+
+lm2 <- lm(Dmel_shapVar ~ sex * condition + log2(leg_tibL), data = Dmel_size)
+
+ggplot(data = Dmel_size, mapping = aes(leg_tibL, Dmel_shapVar, color=sex)) + geom_point()
+
+plot(lm2)
+dwplot(lm2)
+
