2f84551203bfb393d17a3e30d1f23b3a08772b55
Brett-VB <brettvanbakel@gmail.com>
2024-04-01 13:09:13 -0400

    Code update again

--- Prolongata_Melanogaster.R
+++ Prolongata_Melanogaster.R
@@ -10,6 +10,8 @@ Dprol_size <- readRDS("Dprol_size.rds")
 melprol_size <- readRDS("melprol_size.rds")
 
 ##Doing data analysis with melanogaster, might be unnecessary depending on how we end up doing second step of analysis
+
+##PCA melanogaster
 Dmel_trait_size <- select(Dmel_size, leg_tibL, leg_tibW, leg_tar1L, thorax_length_mm) 
 Dmel_PC <- prcomp(Dmel_trait_size)
 
@@ -23,7 +25,8 @@ ggplot(Dmel_trait_size, aes(y = Dmel_PC$x[,2], x = Dmel_PC$x[,3])) +
   xlim(-2, 2) +
   ylim(-2, 2) 
 
-Dmel_shapVar <- Dmel_PC$x[,1]
+##Shape variation melanogaster
+Dmel_shapVar <- Dmel_PC$x[,2]
 
 lm_total_size_Dmel <- lm(Dmel_shapVar ~ sex * condition, data = Dmel_size)
 plot(lm_total_size)
@@ -34,4 +37,6 @@ ggplot(data = Dmel_size, mapping = aes(leg_tibL, Dmel_shapVar, color=sex)) + geo
 
 plot(lm2)
 dwplot(lm2)
+
+Dmel_sizeVar <- Dmel_PC$x[,1] ##PC1 explains size, so this is correct for investigating size?
 
