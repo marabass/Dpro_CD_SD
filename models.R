47576a0564a6747d8b9ae9477589c256e8573370
Mariam Abass <abassm2@mcmaster.ca>
2024-04-01 12:52:57 -0400

    edits -Adding three way interaction between thorax length, condition, and sex

--- models.R
+++ models.R
@@ -6,6 +6,7 @@ library(Matrix)
 library(ggplot2)
 library(tidyverse)
 library(dotwhisker)
+library(performance)
 #install.packages("lme4")
 #install.packages("Matrix")
 
@@ -26,19 +27,22 @@ str(Dprol_size)
 #PC1 as a measure of total shape 
 
 Dprol_trait_size <- select(Dprol_size, leg_tibL, leg_tibW, leg_tar1L, thorax_length_mm) 
-Dprol_PC <- prcomp(Dprol_trait_size) #Not sperated by sex 
+Dprol_PC <- prcomp(Dprol_trait_size) #Not separated by sex 
+summary(Dprol_PC)
 
-ggplot(Dprol_trait_size, aes(y = Dprol_PC$x[,1], x = Dprol_PC$x[,2])) +
+ggplot(Dprol_size, aes(y = Dprol_PC$x[,1], x = Dprol_PC$x[,2], col = sex)) +
   geom_point() +
   xlim(-2, 2) +
-  ylim(-2, 2) 
+  ylim(-2, 2) +
+  labs(x = "PC1 (92.7%)", y = "PC2 (7.2%)")
 
 ggplot(Dprol_trait_size, aes(y = Dprol_PC$x[,2], x = Dprol_PC$x[,3])) +
   geom_point() +
   xlim(-2, 2) +
-  ylim(-2, 2) 
+  ylim(-2, 2) +
+  labs(x = "PC2 (7.2%)", y = "PC3 (7.2%)")
 
-Dprol_shapVar <- Dprol_PC$x[,2]
+Dprol_shapVar <- Dprol_PC$x[,1]
 
 lm_total_size <- lm(Dprol_shapVar ~ sex * condition, data = Dprol_size)
 plot(lm_total_size)
@@ -49,15 +53,53 @@ ggplot(data = Dprol_size, mapping = aes(leg_tibL, Dprol_shapVar, color=sex)) + g
 
 plot(lm2)
 dwplot(lm2)
-# response: tibia length -  lm(leg_log_tibL ~ sex + condtion)
 
-lmm1 <- lmer(leg_log_tibL ~ sex * condition + (1|specimen), data = Dprol_size) # random intercept model 
-lmm2 <- lmer(leg_log_tibL ~ sex * condition + (condition|specimen), data = Dprol_size)
 
+# response: tibia length -  lm(leg_log_tibL ~ sex * condition) - The effect of the interaction between sex and condition on absolute tibia length
+
+ggplot(data = Dprol_size, mapping = aes(condition, leg_log_tibL, group = sex, colour = sex)) + 
+  geom_point() +
+  geom_smooth(method = lm) + 
+  ylab(" Log2 Tibia length (um)")
+
+#Linear model of the interaction between sex and condition on log2-transformed tibia length in micrometers 
+
+lm1 <- lm(leg_log_tibL ~ sex * condition, data = Dprol_size) 
+plot(lm1)
+check_model(lm1)
+confint(lm1)
+
+#Interpreting the intercept: 9.47645184 -  mean tibia length for high condition females 
+
+tibL_HC_F <-  Dprol_size %>%
+  group_by(sex, condition) %>%
+  summarize(mean(leg_log_tibL))
+
+tibL_F <- Dprol_size %>%
+  group_by(sex) %>%
+  summarize(mean(leg_log_tibL))
+
+# sexM coefficient: 0.95287478 - Difference in mean log2 tibia length between high condition females and males 
+# conditionLC coefficient: -0.29397591  - difference in mean log2 tibia length between high condition and low condition females 
+#Interpreting the coefficient of the interaction term: change in log2 tibia length from high condition to low condition for the difference in mean tibia length between males and females.  
+
+#Note leg_log_tibL has been log2 transformed and is in micrometers 
+
+ggplot(data = Dprol_size, mapping = aes(thorax_log_length_mm, leg_log_tibL, colour = sex)) + 
+  geom_point() +
+  facet_wrap(~condition) + 
+  ylab(" Log2 Tibia length (um)")
+
+#Linear model of the effect of the interaction between log2 transformed (and centered) thorax length, sex and condition on log2 transformed tibia length
+lm2 <- lm(leg_log_tibL ~ thorax_log_length_mm * sex * condition, data = Dprol_size) 
+#Interpreting the intercept: 
+
+lmm2 <- lmer(leg_log_tibL ~ sex * condition + (1|condition:specimen), data = Dprol_size)
+isSingular(lmm2)
 #Neither of the mixed models seem to work - trying to incorporate specimen into the model makes the model singular 
 
 ggplot(data = Dprol_size, mapping = aes(condition, leg_log_tibL, color=sex)) + geom_point()
-lmTibL <- lm(leg_log_tibL ~ sex * condition, data = Dprol_size)
+
 plot(lmTibL)
 
 # response: tibia width - lm(leg_log_tibW ~ sex + condition)
