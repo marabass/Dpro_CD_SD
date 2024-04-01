350e0a4f06ad79d58b2c906479ec1afb97946e75
Brett-VB <brettvanbakel@gmail.com>
2024-04-01 11:14:14 -0400

    Melanogaster and Melanoaster+Prolongata data created

--- DataCleaning.R
+++ DataCleaning.R
@@ -5,6 +5,9 @@ DmelFull_size <- read.csv("MP_SpeciesStarvation_Clean.csv")
 
 #extracting D prol data 
 Dprol_size <- DmelFull_size[DmelFull_size$species_full == "D_prolongata",]
+Dmel_size <- DmelFull_size[DmelFull_size$species_full == "D_melanogaster",]
+melprol_size <- rbind(Dprol_size, Dmel_size)
+
 
 #will need to convert character vectors to factors - Should probably have another script that does this (i.e., data clean up)
 Dprol_size$sex <- as.factor(Dprol_size$sex)
@@ -17,3 +20,7 @@ str(Dprol_size)
 Dprol_NoCondition <- Dprol_size[,-19]
 Dprol_male <- Dprol_NoCondition[Dprol_NoCondition$sex == "M",8:20,]
 Dprol_female <- Dprol_NoCondition[Dprol_NoCondition$sex == "F",8:20,]
+
+#Dprol and Dmel data
+Combine_size <- DmelFull_size[DmelFull_size$species_full == "D_melanogaster", "D_prolongata",]
+
