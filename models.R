6b00c5b26a1a2cdeb7a29a73cac88175946db502
Mariam Abass <abassm2@mcmaster.ca>
2024-04-03 15:31:52 -0400

    fixing the mutltivariate mixed model and going back to a multivariate linear model

--- models.R
+++ models.R
@@ -1,6 +1,7 @@
 #Loading packages 
 
 library(lme4)
+library(blme)
 #library(Matrix)
 #library(ggplot2)
 library(tidyverse)
@@ -141,19 +142,55 @@ plot(lmTarW)
 #multivariate linear regression that incorporates all leg traits 
 
 Dprol_trait_full <- select(Dprol_size, leg_tibL, leg_tibW, leg_tar1L, thorax_length_mm, species_full, cohort, sex, specimen, condition) 
-Dprol_trait_size <- select(Dprol_size, leg_tibL, leg_tibW, leg_tar1L, thorax_length_mm)
-Dprol_trait_size_log <- log2(Dprol_trait_size*1000)
+
+Dprol_trait_full$tibL_log2 <- (log2(Dprol_trait_full[,"leg_tibL"])*1000)
+Dprol_trait_full$tibW_log2 <- (log2(Dprol_trait_full[,"leg_tibW"])*1000)
+Dprol_trait_full$tar1L_log2 <- (log2(Dprol_trait_full[,"leg_tar1L"])*1000)
+Dprol_trait_full$thoraxl_log2 <- (log2(Dprol_trait_full[,"thorax_length_mm"])*1000)
+
+
 
 #variance co-variance matrix - checking the correlation between our variables - covariation seems quite low - is this good? 
-cov(Dprol_trait_size)
-pairs(Dprol_trait_size_log)
+cor(Dprol_trait_full[Dprol_trait_full$sex == "M",1:4])
 
+cor(Dprol_trait_full[Dprol_trait_full$sex == "F",1:4])
 
+cor(Dprol_trait_full[,1:4]) # this is not that meaningful, but shows the impact of Simpson's paradox
 
-Ysize <- as.matrix(Dprol_trait_size_log)
-lmMultiv <- lm(Ysize ~ sex * condition, data = Dprol_size)
-class(lmMultiv)
+pairs(Dprol_trait_full[,1:4])
+
+scatterplotMatrix( ~ tibL_log2 + tibW_log2 + tar1L_log2 +thoraxl_log2|sex, 
+                   ellipse = T, data = Dprol_trait_full,
+                   transform = T)
+#fitting a mixed linear model 
+
+Dprol_trait_size <- Dprol_trait_full[,10:13]
+
+Ysize <- as.matrix(Dprol_trait_size)
+lmMultiv <- lm(Ysize ~ sex * condition, data = Dprol_trait_full)
+#class(lmMultiv)
+summary(lmMultiv )
 summary(manova(lmMultiv))
+
+all_traits_ssd <- emmeans(lmMultiv, pairwise ~ sex*condition)
+
+all_traits_ssd_contrasts <- mvcontrast(all_traits_ssd[[1]], interaction = c(condition = "pairwise", sex = "pairwise"),
+                                    mult.name = Ysize)
+
+
+all_traits_ssd_contrasts
+
+confint(all_traits_ssd_contrasts)
+
+
+#The contrasts on display here are the differences in log2 trait size between males and females across high and low condition 
+plot(all_traits_ssd_contrasts) + 
+  geom_vline(xintercept = 0, lty = 2, alpha = 0.5) + 
+  labs(x = "log2 change in SSD in HC vs LC", y = "comparison") +
+  theme_bw()
+
+all_traits_ssd_extract  <- pairs(emmeans(lmm1, 
+                                         specs = ~ sex | condition + trait), simple = "sex", by = "trait")
 
 #converting our data set to the long version 
 head(Dprol_trait_size_log)
@@ -166,11 +203,13 @@ Dprol_long <- (Dprol_trait_full
 
 head(Dprol_long)
 #fitting a linear mixed model 
-lmm1 <- lmer(value ~ trait:(sex * condition) - 1 + (trait-1|specimen), data = Dprol_long, 
+lmm1 <- blmer(value ~ trait:(sex * condition) - 1 + (trait-1|specimen), data = Dprol_long, 
              control = lmerControl(optCtrl=list(ftol_abs=1e-8),
                                    check.nobs.vs.nlev="ignore",
-                                   check.nobs.vs.nRE="ignore"))
+                                  check.nobs.vs.nRE="ignore"))
 
+
+summary(lmm1)
 #par(mfrow=c(1,2))
 vv1 <- VarCorr(lmm1) #somthing wrong here - 
 ## fix unit variance-covariance by adding residual variance:
@@ -179,10 +218,13 @@ corrplot.mixed(cov2cor(vv1$specimen),upper="ellipse")
 
 
 ##contrasts 
+
+
+
 all_traits_ssd <- emmeans(lmm1,  pairwise ~ sex*condition*trait)
 
 all_traits_ssd_contrasts <- contrast(all_traits_ssd[[1]], 
-                                     interaction = c(condtion = "trt.vs.ctrl1", sex = "pairwise"),
+                                     interaction = c(condtion = "pairwise", sex = "pairwise"),
                                      by = "trait")
 
 
@@ -190,9 +232,11 @@ all_traits_ssd_contrasts
 
 confint(all_traits_ssd_contrasts)
 
+
+#The contrasts on display here are the differences in log2 trait size between males and females across high and low condition 
 plot(all_traits_ssd_contrasts) + 
   geom_vline(xintercept = 0, lty = 2, alpha = 0.5) + 
-  labs(x = "change in SSD in after starvation", y = "comparison") +
+  labs(x = "log2 change in SSD in HC vs LC", y = "comparison") +
   theme_bw()
 
 all_traits_ssd_extract  <- pairs(emmeans(lmm1, 
