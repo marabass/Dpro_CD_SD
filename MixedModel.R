90186d6658a1c7e69fcdbe372f14216a1011ead5
Brett-VB <brettvanbakel@gmail.com>
2024-04-24 17:11:08 -0400

    Adding wing and leg comparison model to MixedModel code at the end

--- MixedModel.R
+++ MixedModel.R
@@ -9,8 +9,8 @@ library(DHARMa)
 library(lme4)
 library(broom.mixed)
 library(mvinfluence)
-library(dfoptim)
-library(optimx)
+library(dfoptim) #delete if not needed anymore for current models
+library(optimx) #delete if not needed anymore for current models
 
 
 #load in long D.pro long data set 
@@ -141,3 +141,78 @@ plot(all_traits_CD_contrasts) +
   labs(x = "log2 change in SSD at HC vs LC", y = "comparison") +
   #facet_grid(vars(sex), vars(rep.meas)) + 
   theme_bw()
+
+##MODEL FOR WING AND LEG COMPARISON
+
+#Loading in data
+Dprol_leg_wing <- readRDS("Dprol_leg_wing.rds")
+
+#Fixed effects model
+lm_leg_wing <- lm(cbind(leg_log_tibL, leg_log_tibW, leg_log_tar1L, thorax_log_length_mm, wing_log_area_mm_sq) ~ sex*condition,
+               data = Dprol_leg_wing)
+
+plot(lm_leg_wing)
+influencePlot(lm_leg_wing)
+summary(manova(lm_leg_wing))
+coefLM <- coef(lm_leg_wing)
+confintLM <- confint(lm_leg_wing)
+
+##I'm not sure what this is meant to show, but leaving it in so I have it if needed
+dw_wing <- tidy(lm_leg_wing, conf.int = TRUE)
+
+dw_wing  %>%
+  filter(term != "(Intercept)") %>%
+  #mutate(term = fct_reorder(term, estimate)) %>%
+  ggplot(aes(estimate, term)) +
+  geom_point() +
+  geom_errorbarh(aes(xmin = conf.low, xmax = conf.high), height = .2) +
+  facet_wrap(~response, scale="free",ncol=2) +
+  geom_vline(xintercept=0,lty=2) + 
+  xlab("Effect size") + 
+  ylab("Treatment")
+
+comparison <- emmeans(lm_leg_wing, specs = ~ condition | rep.meas + sex)
+
+#rot_strips <-   theme_bw() +
+  #theme(text = element_text(size = 16),
+        #strip.text.y.right = element_text(angle = 0))
+#this is just visual stuff that can be fixed later
+
+plot(comparison,
+     xlab = "model estimates, trait measurements, log2 transformed")
+
+#not going to keep things pairwise, pairwise is just placeholder for right now
+contrast(comparison, "pairwise")
+
+leg_wing_ssd_contrasts <- contrast(comparison, 
+                                     interaction = c(condition = "pairwise", sex = "pairwise"),
+                                     by = "rep.meas")
+leg_wing_ssd_contrasts
+
+leg_wing_CD_contrasts <- contrast(comparison, 
+                                    method = "pairwise",
+                                    by = c("rep.meas", "sex"))
+
+leg_wing_CD_contrasts
+
+confint(leg_wing_ssd_contrasts)
+
+plot(leg_wing_ssd_contrasts) + 
+  geom_vline(xintercept = 0, lty = 2, alpha = 0.5) + 
+  labs(x = "change in SSD at HC and LC (log2)", y = "comparison") +
+  theme_bw() + theme(text = element_text(size = 16))
+
+#non-pairwise stuff
+comparison_contrasts_ratios <- contrast(comparison, 
+                                        interaction = c(condition = "eff", sex = "pairwise"),
+                                        by = "rep.meas",type = "response")
+
+rot_strips <-   theme_bw() +
+  theme(text = element_text(size = 12),
+        strip.text.y.right = element_text(angle = 0))
+
+plot(comparison_contrasts_ratios) + 
+  geom_vline(xintercept = 0, lty = 2, alpha = 0.5) + 
+  labs(x = "change in SSD at HC and LC (log2)", y = "comparison") +
+  theme_bw() + theme(text = element_text(size = 16)) +
+  rot_strips
